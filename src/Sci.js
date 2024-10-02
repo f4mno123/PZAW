7ca844e7f7b7b1eeac3572930f78badf7f27c19a
f4mno123 <f4mno123@gmail.com>
2024-10-02 09:31:03 +0200

    text handler with useref

--- src/Sci.js
+++ src/Sci.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Sci (){
+
+    let inputRef = useRef();
+
+    const buttonHandler = () => {
+        console.log('hello world');
+        console.log(inputRef);
+
+    }
+
+    const textHandler = (event) => {
+        inputRef = event.target.value;
+    }
+    
+
     return (
         <div>
-            <p>period</p>
+            <p>hdawdwa</p>
+            <input type="button" value="dotknij mnie" onClick={buttonHandler}/>
+            <input  ref={inputRef} type="text" defaultValue="wpisuj" onChange={textHandler} />
+            
         </div>
     );
 }
